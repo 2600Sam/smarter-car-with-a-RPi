0cd20dc0c6b2edd23d45546d5521909218781305
2600Sam <31088491+2600Sam@users.noreply.github.com>
2018-09-06 09:57:16 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -8,48 +8,48 @@ def raise_frame(frame):
     frame.tkraise()
 
 def libre_window():
-        subprocess.call(['libreoffice'])
+        subprocess.call(['libreoffice']) #testing 
 
 def obd_hud():
-        subprocess.call(['python3'] + ['hud3.py'])
+        subprocess.call(['python3'] + ['hud.py'])
 
 def raspi_config():
-        subprocess.call(['sudo'] + ['raspi-config'])
+        subprocess.call(['sudo'] + ['raspi-config']) #raspberry pi only program
 
 def yes_option():
-        subprocess.call(['shutdown'] + ['now'])
+        subprocess.call(['shutdown'] + ['now']) #Linux only "shut it down, shut it down forever" - Mr Book
         #subprocess.call(['sudo'] + ['shutdown'] + ['now']) #need sudo?
 
 menu = tk.Tk()
 
-f1 = tk.Frame(menu)
-f2 = tk.Frame(menu)
+m1 = tk.Frame(menu) #menu page 1
+m2 = tk.Frame(menu) #menu page 2
 b_height = 4
 b_width = 8
 screen_width = menu.winfo_screenwidth()
 screen_height = menu.winfo_screenheight()
 screen_resolution = str(screen_width)+'x'+str(screen_height)
 menu.configure(background= grey)
-menu.geometry(screen_resolution)
+#menu.geometry(screen_resolution) #full screen 
 menu.geometry('800x480') #raspberry pi 7" touch screen emulation 
 menu.resizable(0, 0)
 menu.title("My simple GUI")
 
-for frame in (f1, f2):
+for frame in (m1, m2):
     frame.grid(row=0, column=0, sticky='nsew')
     frame.configure(background=grey)
+#menu page 1
+tk.Label(m1, text='Main Menu', bg = grey).pack(padx=100, pady=20, side='left')
+tk.Button(m1, text = 'OBD HUD', command = obd_hud, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+tk.Button(m1, text = 'Libre Office', command = libre_window, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+tk.Button(m1, text = 'Raspi\nConfig', command = raspi_config, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+tk.Button(m1, text = 'Close', command = menu.quit, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left') #doesn't work in idle but fine in python3 grrrr
+tk.Button(m1, text='Power Off\n The Pi', command=lambda:raise_frame(m2), height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
 
-tk.Label(f1, text='Main Menu', bg = grey).pack(padx=100, pady=20, side='left')
-tk.Button(f1, text = 'OBD HUD', command = obd_hud, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
-tk.Button(f1, text = 'Libre Office', command = libre_window, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
-tk.Button(f1, text = 'Raspi\nConfig', command = raspi_config, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
-tk.Button(f1, text = 'Close', command = menu.quit, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
-tk.Button(f1, text='Power Off\n The Pi', command=lambda:raise_frame(f2), height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+#menu page 2
+tk.Label(m2, text='Please Confirm that Action', bg = grey).pack()
+tk.Button(m2, text = 'Yes\nI\'m Sure\nPower Off', command = yes_option, bg = 'red',activebackground='red', height = b_height, width = b_width, highlightbackground = grey).pack()
+tk.Button(m2, text='NO\nGo Back to\nMain Menu', command=lambda:raise_frame(m1), height = b_height, width = b_width, highlightbackground = grey).pack()
 
-
-tk.Label(f2, text='Please Confirm that Action', bg = grey).pack()
-tk.Button(f2, text = 'Yes\nI\'m Sure\nPower Off', command = yes_option, bg = 'red',activebackground='red', height = b_height, width = b_width, highlightbackground = grey).pack()
-tk.Button(f2, text='NO\nGo Back to\nMain Menu', command=lambda:raise_frame(f1), height = b_height, width = b_width, highlightbackground = grey).pack()
-
-raise_frame(f1)
+raise_frame(m1) #display menu page 1
 menu.mainloop()
