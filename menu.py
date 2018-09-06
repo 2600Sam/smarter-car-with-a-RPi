65d4a6b6a7caccd8128f4e9fba7a20693f2108ba
2600Sam <31088491+2600Sam@users.noreply.github.com>
2018-09-06 15:50:42 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -22,8 +22,8 @@ def yes_option():
 
 menu = tk.Tk()
 
-m1 = tk.Frame(menu) #menu page 1
-m2 = tk.Frame(menu) #menu page 2
+menu_frame1 = tk.Frame(menu) #menu page 1
+menu_frame2 = tk.Frame(menu) #menu page 2
 b_height = 4
 b_width = 8
 screen_width = menu.winfo_screenwidth()
@@ -35,21 +35,21 @@ menu.geometry('800x480') #raspberry pi 7" touch screen emulation
 menu.resizable(0, 0)
 menu.title("My simple GUI")
 
-for frame in (m1, m2):
+for frame in (menu_frame1, menu_frame2):
     frame.grid(row=0, column=0, sticky='nsew')
     frame.configure(background=grey)
 #menu page 1
-tk.Label(m1, text='Main Menu', bg = grey).pack(padx=100, pady=20, side='left')
-tk.Button(m1, text = 'OBD HUD', command = obd_hud, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
-tk.Button(m1, text = 'Navit\nGPS', command = navit_window, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
-tk.Button(m1, text = 'Raspi\nConfig', command = raspi_config, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
-tk.Button(m1, text = 'Close', command = menu.quit, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left') #doesn't work in idle but fine in python3 grrrr
-tk.Button(m1, text='Power Off\n The Pi', command=lambda:raise_frame(m2), height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+tk.Label(menu_frame1, text='Main Menu', bg = grey).pack(padx=100, pady=20, side='left')
+tk.Button(menu_frame1, text = 'OBD HUD', command = obd_hud, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+tk.Button(menu_frame1, text = 'Navit\nGPS', command = navit_window, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+tk.Button(menu_frame1, text = 'Raspi\nConfig', command = raspi_config, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
+tk.Button(menu_frame1, text = 'Close', command = menu.quit, height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left') #doesn't work in idle but fine in python3 grrrr
+tk.Button(menu_frame1, text='Power Off\n The Pi', command=lambda:raise_frame(menu_frame2), height = b_height, width = b_width, highlightbackground = grey).pack(padx=5, pady=20, side='left')
 
 #menu page 2
-tk.Label(m2, text='Please Confirm that Action', bg = grey).pack()
-tk.Button(m2, text = 'Yes\nI\'m Sure\nPower Off', command = yes_option, bg = 'red',activebackground='red', height = b_height, width = b_width, highlightbackground = grey).pack()
-tk.Button(m2, text='NO\nGo Back to\nMain Menu', command=lambda:raise_frame(m1), height = b_height, width = b_width, highlightbackground = grey).pack()
+tk.Label(menu_frame2, text='Please Confirm that Action', bg = grey).pack()
+tk.Button(menu_frame2, text = 'Yes\nI\'m Sure\nPower Off', command = yes_option, bg = 'red',activebackground='red', height = b_height, width = b_width, highlightbackground = grey).pack()
+tk.Button(menu_frame2, text='NO\nGo Back to\nMain Menu', command=lambda:raise_frame(menu_frame1), height = b_height, width = b_width, highlightbackground = grey).pack()
 
-raise_frame(m1) #display menu page 1
+raise_frame(menu_frame1) #display menu page 1
 menu.mainloop()
