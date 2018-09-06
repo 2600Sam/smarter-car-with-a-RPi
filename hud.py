0095f11256de04dc99b548243ba9798a4b1acdf1
2600Sam <31088491+2600Sam@users.noreply.github.com>
2018-09-06 09:37:30 +0800

    Update hud.py

--- hud.py
+++ hud.py
@@ -6,11 +6,12 @@ import obd
 from obd import OBDStatus
 
 pygame.init()
-
-#connection = obd.OBD()
+#all OBD connections are commented out so it will work (testing mode)
+#connection = obd.OBD() 
 #connect = obd.Async(fast=False)
 
-screen = pygame.display.set_mode((800,480)) #,pygame.FULLSCREEN)
+#screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN) #use for full screen
+screen = pygame.display.set_mode((800,480)) #set 800x480 for smaller raspi 7" screen
 screen_w = screen.get_width()
 screen_h = screen.get_height()
 circle_y = screen_h / 2
@@ -54,7 +55,7 @@ posit = []
 #pie is exactly 3
 raspberry = 3.141592653
 
-class button():
+class button(): #the exit button this should work!
 	def __init__(self, color, x,y,width,height, text=''):
 		self.color = color
 		self.x = x
@@ -81,7 +82,7 @@ class button():
 		return False
 
 
-for s in range (0,60):
+for s in range (0,60): #fill the posit[] with clock coordinates 60 x,y'zies (maybe faster than doing the math all the time...maybe)
 	pos = [int(math.cos(math.radians(s*6+270))*time_radian+circle4_x),int(math.sin(math.radians(s*6+270))*time_radian+circle_y2)]
 	posit.append(pos)
 
@@ -98,9 +99,9 @@ def getSec():
 def draw_hud():
 	screen.fill(grey)
 
-	pygame.draw.circle(screen, black, (int(circle1_x), int(circle_y)), int(circle_rad), 5)
+	pygame.draw.circle(screen, black, (int(circle1_x), int(circle_y)), int(circle_rad), 5) #draw rpm gague
 	for step in range (0,9000,500):
-		vcolor = white
+		vcolor = white #do color changes for higher rpm range (change as required for your car)
 		if step >= 4500:
 			vcolor = yellow
 		if step >= 6000:
@@ -113,8 +114,7 @@ def draw_hud():
 		screen.blit(number_text, number_text_loc)
 		pygame.draw.circle(screen, vcolor, [int(line_x), int(line_y)], 2)
 
-	pygame.draw.circle(screen, black, (int(circle2_x), int(circle_y)), int(circle_rad), 5)
-
+	pygame.draw.circle(screen, black, (int(circle2_x), int(circle_y)), int(circle_rad), 5) #draw speed gague
 	for step in range (0,150,10):
 		angle1 = (angle - ((step / .031) / 1000))
 		line_x = (circle_rad - 8) * math.sin(angle1) + circle2_x
@@ -124,8 +124,7 @@ def draw_hud():
 		number_text_loc = number_text.get_rect(center=(line_x, line_y+8))
 		screen.blit (number_text, number_text_loc)
 
-	pygame.draw.circle(screen, black, (int(circle3_x), int(circle_y)), int(circle_rad), 5)
-
+	pygame.draw.circle(screen, black, (int(circle3_x), int(circle_y)), int(circle_rad), 5) #draw load gague
 	for step in range (0,110,10):
 		angle3 = (angle - ((step / .022) / 1000))
 		line_x = (circle_rad - 8) * math.sin(angle3) + circle3_x
@@ -135,7 +134,26 @@ def draw_hud():
 		screen.blit(number_text, number_text_loc)
 		pygame.draw.circle(screen, white, [int(line_x), int(line_y)], 2)
 
-	speed_text = headerFont.render('SPEED', True, black)
+	pygame.draw.circle(screen, black, (int(circle5_x), int(circle_y2)), (int(time_radian)), 5) #draw the fuel gague
+	for step in range(0,110,25):
+		angle5 = (angle - ((step / .022) / 1000))
+		line_x = (time_radian - 8) * math.sin(angle5) + circle5_x
+		line_y = (time_radian - 8) * math.cos(angle5) + circle_y2
+		if step == 0:
+			number_text = numberFont.render('E', True, white)
+		elif step == 25:
+			number_text = numberFont.render('1/4', True, white)
+		elif step == 50:
+			number_text = numberFont.render("1/2", True, white)
+		elif step == 75:
+			number_text = numberFont.render("3/4", True, white)
+		else:
+			number_text = numberFont.render("F", True, white)
+		number_text_loc = number_text.get_rect(center=(line_x, line_y+8))
+		screen.blit(number_text, number_text_loc)
+		pygame.draw.circle(screen, white, [int(line_x), int(line_y)], 2)
+
+	speed_text = headerFont.render('SPEED', True, black) #do the labels
 	rpm_text = headerFont.render('RPM', True, black)
 	load_text = headerFont.render('LOAD', True, black)
 	oil_text = timeFont.render('OIL', True, black)
@@ -162,6 +180,11 @@ def draw_hud():
 	screen.blit(temp_text, temp2_text_loc)
 	screen.blit(otemp_text, otemp_text_loc)
 	
+	exitButton.draw(screen, (0,0,0))
+
+
+def draw_clock(): #analog then digital
+	
 	if int(getHour())==12:
 		hourX = posit[0*5 + int(int(getMin())/12)][0]
 		hourY = posit[0*5 + int(int(getMin())/12)][1]
@@ -175,32 +198,12 @@ def draw_hud():
 	pygame.draw.line(screen,white,(circle4_x,circle_y2),(hourX,hourY),8)
 	pygame.draw.line(screen,offwhite,(circle4_x,circle_y2),(minX),4)
 	pygame.draw.circle(screen,red,(circle4_x,circle_y2),7,0)
-	pygame.draw.line(screen,red,(circle4_x,circle_y2),(secX),1)
-	pygame.draw.circle(screen, black, (int(circle4_x), int(circle_y2)), (int(time_radian)+4), 5)
+	pygame.draw.line(screen,red,(circle4_x,circle_y2),(secX),1) #put a dot in the center to cover up the start of the lines
+	pygame.draw.circle(screen, black, (int(circle4_x), int(circle_y2)), (int(time_radian)+4), 5) #draw the outer circle last to cover the end point of the lines 
 
-	time_text = timeFont.render(getHour()+':'+getMin()+':'+getSec(), True, white)
+	time_text = timeFont.render(getHour()+':'+getMin()+':'+getSec(), True, white) #digital clock 
 	time_text_loc = time_text.get_rect(center=(circle4_x, circle_y2 + (15*3)))
 	screen.blit(time_text, time_text_loc)
-
-	pygame.draw.circle(screen, black, (int(circle5_x), int(circle_y2)), (int(time_radian)), 5)
-	for step in range(0,110,25):
-		angle5 = (angle - ((step / .022) / 1000))
-		line_x = (time_radian - 8) * math.sin(angle5) + circle5_x
-		line_y = (time_radian - 8) * math.cos(angle5) + circle_y2
-		if step == 0:
-			number_text = numberFont.render('E', True, white)
-		elif step == 25:
-			number_text = numberFont.render('1/4', True, white)
-		elif step == 50:
-			number_text = numberFont.render("1/2", True, white)
-		elif step == 75:
-			number_text = numberFont.render("3/4", True, white)
-		else:
-			number_text = numberFont.render("F", True, white)
-		number_text_loc = number_text.get_rect(center=(line_x, line_y+8))
-		screen.blit(number_text, number_text_loc)
-		pygame.draw.circle(screen, white, [int(line_x), int(line_y)], 2)
-		exitButton.draw(screen, (0,0,0))
 
 def get_speed(s):
 	global speed
@@ -229,6 +232,7 @@ def get_otemp(ot):
 	if not ot.is_null():
 		otemp = int(ot.value.mangitude) #for celsisus
 		#otemp = int(ot.value.mangitude * 9 / 5 + 32) #for fahrenheit
+		
 #connection.watch(obd.commands.SPEED, callback=get_speed)
 #connection.watch(obd.commands.RPM, callback=get_rpm)
 #connection.watch(obd.commands.ENGINE_LOAD, callback=get_load)
@@ -240,7 +244,7 @@ def get_otemp(ot):
 running = True
 exitButton = button(red, 10, 10, 50, 25, 'Exit')
 while running:
-	for event in pygame.event.get():
+	for event in pygame.event.get(): #exit back to main menu with esc or click exit button
 			pos = pygame.mouse.get_pos()
 			if event.type == KEYDOWN:
 				if event.key == K_ESCAPE:
@@ -256,14 +260,14 @@ while running:
 					running = False
 	draw_hud()
 
-	speedDisplay = digitFont.render(str(int(speed)), 3, white)
+	speedDisplay = digitFont.render(str(int(speed)), 3, white) #display the speed analog
 	angle1 = (angle - ((speed / .031) / 1000))
 	line_x = (circle_rad - 10) * math.sin(angle1) + circle2_x
 	line_y = (circle_rad - 10) * math.cos(angle1) + circle_y
 	pygame.draw.line(screen, blue, [circle2_x, circle_y], [line_x, line_y], 4)
 
-	rpmDisplay = digitFont.render(str(rpm), 3, white)
-	if rpm >= 4500:
+	rpmDisplay = digitFont.render(str(rpm), 3, white) #display the rpm analog with color changeing line!)
+	if rpm >= 4500: #change color for highrer rpm adjust as required for your car
 				vcolor = yellow
 	if rpm >= 6000:
 				vcolor = red
@@ -272,29 +276,37 @@ while running:
 	line_y = (circle_rad - 10) * math.cos(angle2) + circle_y
 	pygame.draw.line(screen, vcolor, [circle1_x, circle_y], [line_x, line_y], 2)
 
-	loadDisplay = digitFont.render(" " + str(int(load)) + '%', 3, white)
+	loadDisplay = digitFont.render(" " + str(int(load)) + '%', 3, white) #display the calculated load analog
 	angle3 = (angle - ((load / .022) / 1000)) 
 	line_x = (circle_rad - 10) * math.sin(angle3) + circle3_x
 	line_y = (circle_rad - 10) * math.cos(angle3) + circle_y
 	pygame.draw.line(screen, red, [circle3_x, circle_y], [line_x, line_y], 6)
 
-	angle5 = (angle - ((fuel / .022) / 1000))
+	angle5 = (angle - ((fuel / .022) / 1000)) #display the fuel analog (no digital for fuel)
 	line_x = (time_radian - 10) * math.sin(angle5) + circle5_x
 	line_y = (time_radian - 10) * math.cos(angle5) + circle_y2
 	pygame.draw.line(screen, green, [circle5_x, circle_y2], [line_x, line_y], 3)
 
-	screen.blit(loadDisplay, (circle3_x -((circle3_x / 8) - 55), circle_y + 60))
+	screen.blit(loadDisplay, (circle3_x -((circle3_x / 8) - 55), circle_y + 60)) #do the 3 digital displays
 	screen.blit(rpmDisplay, (circle1_x - (circle2_x / 8) + 25, circle_y + 60))
 	screen.blit(speedDisplay,(circle2_x -(circle1_x / 8) + 10, circle_y + 60))
+
+	draw_clock()
+	
 	pygame.display.update()
 	pygame.display.flip()
+
+	#added to show movement 
 	rpm += 5
 	if rpm >= 8505:
 			rpm = 0
 	speed += .05
+	wtemp = int(speed)*2
+	otemp = wtemp
 	if speed >= 140:
 			speed = 0
 	load += .05
+	fuel = load
 	if load >= 101:
 			load = 0
 pygame.quit()
